452817963d72fe643a75c6483a0ffd9648f4e870
qtr-deagle <cjalamares05@gmail.com>
2025-06-14 13:03:54 +0800

    Search Bar and Tags Working

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,24 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing 
+
 // All Listings
-Route::get('/', function () {
-    return view('Listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all(),
-    ]);
-});
+Route::get('/', [ListingController ::class, 'index']);
+
+// Create Listing
+Route::get('/listings/create', [ListingController ::class, 'create']);
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [listingController::class, 'show']);
